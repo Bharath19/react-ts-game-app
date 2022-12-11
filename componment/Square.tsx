c05cd738a7fab1f222a2b7f2302c59400de67dc1
Bharath19 <bharathganesan17@gmail.com>
2022-12-11 14:58:48 +0100

    Completing the Game

--- componment/Square.tsx
+++ componment/Square.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 
 export default function Square(props) {
-  const [stateValue, setStateValue] = React.useState({ value: props.value });
-
   return (
-    <button className="square" onClick={() => setStateValue({ value: 'X' })}>
-      {stateValue.value}
+    <button className="square" onClick={props.onClick}>
+      {props.value}
     </button>
   );
 }
